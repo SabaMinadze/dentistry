aa029f97bb1882ab33e66c41d809cb4a08bff484
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-07-15 13:22:37 +0400

    Add responsiveness to main page.tsx page.

---
+++ app/components/Burgenav/Burgernav.tsx
@@ -0,0 +1,30 @@
+// components/HamburgerMenu.js
+import Link from 'next/link';
+import styles from './Burgernav.module.css'
+
+const Burgernav = () => (
+  <>
+    <label className={styles.hamburgerMenu}>
+            <input type="checkbox" />
+          </label>
+          <aside className={styles.sidebar}>
+            <nav>
+            <Link href="/aboutus" className={styles.txt}><div>ჩვენს შესახებ</div></Link>
+            <br/>
+            <Link href="/stomatologi" className={styles.txt}><div>ჩვენი სტომატოლოგები</div></Link>
+            <br/>
+            <Link href="/achievements" className={styles.txt}><div>ჩვენი მიღწევები</div></Link>
+            <br/>
+            <Link href="/residency" className={styles.txt}><div>რეზიდენტურა</div></Link>
+            <br/>
+            <Link href="/questions" className={styles.txt}><div>კითხვები</div></Link>
+            <br/>
+            <Link href="/servicesandprices" className={styles.txt}><div>სერვისები და ფასები</div></Link>
+            <br/><br/><br/><br/>
+            <Link href="/contactus" className={styles.txt2}><div>კონტაქტი</div></Link>
+            </nav>
+          </aside>
+  </>
+);
+
+export default Burgernav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Subnav from './components/Subnav/Subnav'
+import Burgernav from './components/Burgenav/Burgernav'
+import styles from './page.module.css'
 
 function page() {
   return (
     <>
-      <Subnav/>
+      <div className={styles.box}>
+          <Subnav/>
+          <Burgernav/>
+      </div>
     </>
   )
 }
