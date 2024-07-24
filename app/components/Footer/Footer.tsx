ff7b8ea139b90dd24169120f95c718b983f96c74
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-07-24 16:32:56 +0400

    Update Footer.tsx

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Link from 'next/link'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <h1 className={styles.txt}>MADE BY SABA MINADZE</h1>
+      <h2 className={styles.txt3}>Instagram: minadzesaba16</h2>
+      <br/><br/>
+      <h1>Language</h1>
+      <Link href=""><button className={styles.btn}>English</button></Link>
+
+      <h2 className={styles.txt2}>algandent.ge ყველა უფლება დაცულია. © 2023 - 2024</h2>
+
+
+    </div>
   )
 }
 
