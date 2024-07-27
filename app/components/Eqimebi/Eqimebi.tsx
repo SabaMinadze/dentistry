6cebd65e97835884aaf16b9b1a484b9f9cbf89db
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-07-27 15:09:02 +0400

    Change media queries

--- app/components/Eqimebi/Eqimebi.tsx
+++ app/components/Eqimebi/Eqimebi.tsx
@@ -11,7 +11,7 @@ function Eqimebi() {
       <Card2 name="ანა ვოსკანიანი"
               prof="ექიმი სტომატოლოგი თერაპევტი"
                 prof2="პირის ღრუს ქორურგი"
-                img={<Image src="/eqimi.jpg" height={150} width={150} alt="eror"/>}
+                img={<Image src="/eqimi.jpg" height={150} width={120} alt="eror"/>}
                   duty1="იმპლანტაცია"
                   duty2="სთეტიური სტომატოლოგია"
                   duty3="ღრძილების ქირურგიული პლასტიკა"
@@ -21,7 +21,7 @@ function Eqimebi() {
       <Card2 name="ლანა ჭულუხაძე"
               prof="თერაპევტი ქირურგი"
               prof2="სტომატოლოგი"
-                img={<Image src="/eqimi2.jpg" height={150} width={150} alt="eror"/>}
+                img={<Image src="/eqimi2.jpg" height={150} width={120} alt="eror"/>}
                   duty1="კარიესის მკურნალობა"
                   duty2="თანამედროვე ერთოდონტიური მკურნალობა"
                   duty3="მხატვრული რესტავრაცია"
