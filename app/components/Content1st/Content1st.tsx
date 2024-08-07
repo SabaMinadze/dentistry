30312e517af2d966334df0082bc96c44b00fae46
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-08-07 23:42:51 +0400

    Working hard... pt2

--- app/components/Content1st/Content1st.tsx
+++ app/components/Content1st/Content1st.tsx
@@ -8,25 +8,28 @@ import KitxvadaTema from '../KitxvadaTema/KitxvadaTema'
 function Content1st() {
   return (
     <>
-        <center><Link href="/">
-          <Image src="/dentistrylogo.jpg"
-          height={170} 
-          width={170} 
-          alt="eror" 
-          className={styles.logo}/>
-        </Link></center>
-      <div className={styles.content}>
-        <div className={styles.box}>
-        <center><h1 className={styles.maintxt}>დენრტისტრი</h1></center>
-        <br/><br/><br/>
-        <KitxvadaTema/>
-        <br/>
-          <h1 className={styles.txt}>მოგესალმებით ჩვენ ვართ ნომერ პირველი და მთავარი წამყვანი სტომატოლგიური კლინიკა 
-            მთელ საქართველოში</h1>
-  
-          
-          <h1 className={styles.saba}>ჩვენი ექიმები:</h1>
-        </div>
+    <div className={styles.dzaanbigbox}>
+    <center><Link href="/">
+                <Image src="/dentistrylogo.jpg"
+                height={170} 
+                width={170} 
+                alt="eror" 
+                className={styles.logo}/>
+              </Link></center>
+            <div className={styles.content}>
+              <div className={styles.box}>
+              <center><h1 className={styles.maintxt}>დენრტისტრი</h1></center>
+              <br/><br/><br/>
+              <KitxvadaTema/>     
+      </div>
+ 
+            <br/>
+              <h1 className={styles.txt}>მოგესალმებით ჩვენ ვართ ნომერ პირველი და მთავარი წამყვანი სტომატოლგიური კლინიკა 
+                მთელ საქართველოში</h1>
+      
+              
+              <h1 className={styles.saba}>ჩვენი ექიმები:</h1>
+            </div>
       </div>
     </>
   )
